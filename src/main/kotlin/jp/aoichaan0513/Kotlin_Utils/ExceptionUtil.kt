3460d29e91c605cc4a3f18af22d6f78bea70cb24
Aoichaan0513 <aoichaan0513@gmail.com>
2022-07-01 10:20:45 +0900

    [Fix] ExceptionUtil.kt の拡張関数名を変更

--- src/main/kotlin/jp/aoichaan0513/Kotlin_Utils/ExceptionUtil.kt
+++ src/main/kotlin/jp/aoichaan0513/Kotlin_Utils/ExceptionUtil.kt
@@ -3,13 +3,13 @@ package jp.aoichaan0513.Kotlin_Utils
 import java.io.PrintWriter
 import java.io.StringWriter
 
-val Exception.message
+val Exception.stacktraceMessage
     get() = StringWriter().use {
         PrintWriter(it).use { printStackTrace(it) }
         it.toString()
     }
 
-val Throwable.message
+val Throwable.stacktraceMessage
     get() = StringWriter().use {
         PrintWriter(it).use { printStackTrace(it) }
         it.toString()
